2346de0949c9da6a4c2e164ef18eb0b6f03e94b0
Abhay Patel <abhaypatel0286@gmail.com>
2026-09-13 08:49:04 +0530

    Time: 1 ms (100%), Space: 42.8 MB (34.07%) - LeetHub

---
+++ 3789-minimum-cost-to-acquire-required-items/3789-minimum-cost-to-acquire-required-items.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
+        long min = Math.min(need1, need2);
+        long max = Math.max(need1, need2);
+        long ans = (long) need1 * cost1 + (long) need2 * cost2;
+        ans  = Math.min(ans, (long)min * costBoth + (long) (need1 - min) * cost1 + (long) (need2 - min) * cost2);
+        ans = Math.min(ans, (long) max * costBoth);
+        return ans;   
+    }
+}
